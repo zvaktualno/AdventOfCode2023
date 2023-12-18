0dbb22ad8c258ecacbfa7363736c015a063c5c44
zvaktualno <tadej.zivic@hotmail.com>
2023-12-18 16:48:09 +0100

    Day 16 part 2

--- aoc_day_16/src/first.rs
+++ aoc_day_16/src/first.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 
 #[derive(Eq, PartialEq, Clone)]
 struct Point {
@@ -13,7 +15,6 @@ fn next(field: &Vec<Vec<char>>, position: &Point, prev_position: &Point) -> Vec<
     let ch = field[position.y as usize][position.x as usize];
     // Going right
     if position.x - prev_position.x > 0 {
-        //println!("Going right");
         return match ch {
             '|' => vec![(Point {x: position.x, y:position.y - 1}, UP), (Point {x: position.x, y:position.y + 1}, DOWN)],
             '-' => vec![(Point {x: position.x + 1, y:position.y}, RIGHT)],
@@ -25,7 +26,6 @@ fn next(field: &Vec<Vec<char>>, position: &Point, prev_position: &Point) -> Vec<
     }
     // Going left
     else if position.x - prev_position.x < 0 {
-        //println!("Going left");
         return match ch {
             '|' => vec![(Point {x: position.x, y:position.y - 1}, UP), (Point {x: position.x, y:position.y + 1}, DOWN)],
             '-' => vec![(Point {x: position.x - 1, y:position.y}, LEFT)],
@@ -37,7 +37,6 @@ fn next(field: &Vec<Vec<char>>, position: &Point, prev_position: &Point) -> Vec<
     }
     // Going down
     else if position.y - prev_position.y > 0 {
-        //println!("Going down");
         return match ch {
             '|' => vec![(Point {x: position.x, y:position.y + 1}, DOWN)],
             '-' => vec![(Point {x: position.x - 1, y:position.y}, LEFT), (Point {x: position.x + 1, y:position.y},RIGHT)],
@@ -49,7 +48,6 @@ fn next(field: &Vec<Vec<char>>, position: &Point, prev_position: &Point) -> Vec<
     }
     // Going up
     else if position.y - prev_position.y < 0 {
-        //println!("Going up");
         return match ch {
             '|' => vec![(Point {x: position.x, y:position.y - 1}, UP)],
             '-' => vec![(Point {x: position.x - 1, y:position.y}, LEFT), (Point {x: position.x + 1, y:position.y},RIGHT)],
@@ -68,29 +66,35 @@ fn point_valid(point: &Point, xy_max: &Point) -> bool {
 
 fn get(field: &Vec<Vec<char>>, score: &mut Vec<Vec<u8>>, position: &Point, prev_position: &Point, deep: u32){
 
+    // Get all next possible points
     let mut points = next(field, &position, &prev_position);
     let field_size = Point {x: field[0].len() as i32, y: field.len() as i32};
 
+    // Mark the current position with all directions of these points, checking if the direction was already taken. If that happens, this node was already checked and the function can return.
     for (_, dir) in &points{
         if score[position.y as usize][position.x as usize] & dir > 0{
             return;
         }
         score[position.y as usize][position.x as usize] |= dir;
-    }   
+    }
+
+    // Filter out all points that are outside the field.
     points = points.into_iter().filter(|(point, _dir)| point_valid(point, &field_size)).collect();
-    
+
+    // For every valid point, mark all spots untill the next mirror is found.    
     'outer: for (point, dir) in points{
 
-        
         let mut prev_possible_point = position.clone();
         let mut possible_point = point;
         
         loop {
             score[prev_possible_point.y as usize][prev_possible_point.x as usize] |= dir;
+
+            // If the path is leading outside the field, go process the next point.
             if !point_valid(&possible_point, &field_size){
                 continue 'outer;
             }
-            // If next point is a dot
+            // If next point is a dot, check next point
             if field[possible_point.y as usize][possible_point.x as usize] == '.'{
                 let (np, d) = next(field, &possible_point, &prev_possible_point)[0].clone();
                 prev_possible_point = possible_point;
@@ -101,32 +105,25 @@ fn get(field: &Vec<Vec<char>>, score: &mut Vec<Vec<u8>>, position: &Point, prev_
                 break;
             }
         }
+
+        // If a mirror is found, process it recursevly.
         get(field, score, &possible_point, &prev_possible_point, deep+1);
-       
     }
 }
 
 pub fn solve(lines: &Vec<String>) {
+    // Process input
     let field = lines.into_iter().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
-    let mut field_bool: Vec<Vec<u8>> = vec![vec![0; field[0].len()]; field.len()];
+
+    // Prepare the score field
+    let mut score: Vec<Vec<u8>> = vec![vec![0; field[0].len()]; field.len()];
     
+    // Determine the starting points
     let starting_point = Point {x: 0, y: 0};
     let prev_point = Point{x: -1, y: 0};
-    get(&field, &mut field_bool, &starting_point, &prev_point, 0);
+    get(&field, &mut score, &starting_point, &prev_point, 0);
 
-    let res: usize = field_bool.iter().map(|row| row.into_iter().filter(|x| **x>0).count()).sum();
-    for i in field_bool{
-        for a in i{
-            match a {
-                DOWN => {print!(" v ");},
-                UP => {print!(" ^ ");},
-                LEFT => {print!(" < ");},
-                RIGHT => {print!(" > ");},
-                0 => print!(" . "),
-                x => print!("{x:#2} "),
-            }
-        }
-        println!("");
-    }
+    let res: usize = score.iter().map(|row| row.into_iter().filter(|x| **x>0).count()).sum();
+
     println!("Part one result {}", res);
 }
